6208cd19f198bd41c88bf0763a6054c3501cca9c
jackvdp <jack@pumpymusic.co.uk>
2024-10-09 16:19:20 +0100

    Change text on parallax

--- src/components/Parallax.jsx
+++ src/components/Parallax.jsx
@@ -4,7 +4,7 @@ export default function Parallax() {
             <div className="container">
                 <div className="row mt-10 mb-10">
                     <div className="col-12 col-xxl-7 col-xl-8">
-                        <h2 className="text-dark-gray text-white alt-font fw-500 ls-minus-2px w-95 md-w-100" data-fancy-text='{ "opacity": [0, 1], "translateY": [50, 0], "filter": ["blur(20px)", "blur(0px)"], "string": ["Forward thinking team of designers, copywriters and developers."], "duration": 400, "delay": 0, "speed": 20, "easing": "easeOutQuad" }'></h2>
+                        <h2 className="text-dark-gray text-white alt-font fw-500 ls-minus-2px w-95 md-w-100" data-fancy-text='{ "opacity": [0, 1], "translateY": [50, 0], "filter": ["blur(20px)", "blur(0px)"], "string": ["Creating tailored, cost-effective digital solutions for your vision."], "duration": 400, "delay": 0, "speed": 20, "easing": "easeOutQuad" }'></h2>
                     </div>
                 </div>
             </div>
