1349dee355bc20c86a8e8024bd75c6c90722b870
jackvdp <jack@pumpymusic.co.uk>
2024-10-09 11:17:54 +0100

    Continue refactor of case studies

--- src/components/CaseStudies/index.jsx
+++ src/components/CaseStudies/index.jsx
@@ -38,31 +38,37 @@ function Icons() {
             <div className="col-12 filter-content p-lg-0">
                 <ul className="portfolio-attractive portfolio-wrapper grid-loading grid grid-3col xxl-grid-3col xl-grid-3col lg-grid-3col md-grid-2col sm-grid-2col xs-grid-1col gutter-large text-center">
                     <li className="grid-sizer"></li>
-                    {caseStudies.map(item => (
-                        <li key={item.id} className={`grid-item ${item.className} transition-inner-all`} data-atropos data-atropos-perspective="1450">
-                            <div className="position-relative">
-                                <a href={item.link} className="portfolio-link"></a>
-                                <div className="atropos-scale">
-                                    <div className="atropos-rotate">
-                                        <div className="atropos-inner" data-atropos-offset="3">
-                                            <div className={`portfolio-box ${item.bgClass}`}>
-                                                <div className="portfolio-image">
-                                                    <img src={item.imgSrc} alt="" />
-                                                </div>
-                                                <div className="portfolio-hover justify-content-end align-items-center d-flex flex-column pt-40px pb-40px sm-pt-30px sm-pb-30px">
-                                                    <span className="icon-box z-index-1 mb-auto ms-auto me-30px"><i className="bi bi-arrow-up-right icon-very-medium text-white" aria-hidden="true"></i></span>
-                                                    <div className="text-white fs-22 move-top-bottom-self"><span>{item.title}</span></div>
-                                                    <div className="fs-15 lh-28 text-white opacity-6 move-bottom-top-self"><span>{item.subtitle}</span></div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </li>
+                    {caseStudies.map((item, index) => (
+                        <CaseStudy key={index} {...item} />
                     ))}
                 </ul>
             </div>
         </div>
+    )
+}
+
+function CaseStudy(item) {
+    return (
+        <li key={item.id} className={`grid-item ${item.className} transition-inner-all`} data-atropos data-atropos-perspective="1450">
+            <div className="position-relative">
+                <a href={item.link} className="portfolio-link"></a>
+                <div className="atropos-scale">
+                    <div className="atropos-rotate">
+                        <div className="atropos-inner" data-atropos-offset="3">
+                            <div className={`portfolio-box ${item.bgClass}`}>
+                                <div className="portfolio-image">
+                                    <img src={item.imgSrc} alt="" />
+                                </div>
+                                <div className="portfolio-hover justify-content-end align-items-center d-flex flex-column pt-40px pb-40px sm-pt-30px sm-pb-30px">
+                                    <span className="icon-box z-index-1 mb-auto ms-auto me-30px"><i className="bi bi-arrow-up-right icon-very-medium text-white" aria-hidden="true"></i></span>
+                                    <div className="text-white fs-22 move-top-bottom-self"><span>{item.title}</span></div>
+                                    <div className="fs-15 lh-28 text-white opacity-6 move-bottom-top-self"><span>{item.subtitle}</span></div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </li>
     )
 }
