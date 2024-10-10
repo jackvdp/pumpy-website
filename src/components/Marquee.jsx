7b12601d0e203dc932498a93894f658c9b9a5553
jackvdp <jack@pumpymusic.co.uk>
2024-10-10 15:33:59 +0100

    Extract marquee section

---
+++ src/components/Marquee.jsx
@@ -0,0 +1,32 @@
+export default function Marquee() {
+    return (
+        <section className="half-section pb-0">
+            <div className="container-fluid">
+                <div className="row position-relative">
+                    <div className="col swiper swiper-width-auto text-center pb-20px md-pb-20px" data-slider-options='{ "slidesPerView": "auto", "spaceBetween":40, "speed": 8000, "loop": true, "allowTouchMove": false, "autoplay": { "delay":0, "disableOnInteraction": false }, "keyboard": { "enabled": true, "onlyInViewport": true }, "effect": "slide" }'>
+                        <div className="swiper-wrapper marquee-slide">
+                            <div className="swiper-slide">
+                                <div className="fs-140 lg-fs-130 md-fs-110 sm-fs-90 text-dark-gray fw-600 ls-minus-6px alt-font">marketing.</div>
+                            </div>
+                            <div className="swiper-slide">
+                                <div className="fs-140 lg-fs-130 md-fs-110 sm-fs-90 fw-600 text-outline text-outline-color-extra-medium-gray ls-minus-6px alt-font">analysis.</div>
+                            </div>
+                            <div className="swiper-slide">
+                                <div className="fs-140 lg-fs-130 md-fs-110 sm-fs-90 text-dark-gray fw-600 ls-minus-6px alt-font">strategy.</div>
+                            </div>
+                            <div className="swiper-slide">
+                                <div className="fs-140 lg-fs-130 md-fs-110 sm-fs-90 fw-600 text-outline text-outline-color-extra-medium-gray ls-minus-6px alt-font">business.</div>
+                            </div>
+                            <div className="swiper-slide">
+                                <div className="fs-140 lg-fs-130 md-fs-110 sm-fs-90 text-dark-gray fw-600 ls-minus-6px alt-font">marketing.</div>
+                            </div>
+                            <div className="swiper-slide">
+                                <div className="fs-140 lg-fs-130 md-fs-110 sm-fs-90 fw-600 text-outline text-outline-color-extra-medium-gray ls-minus-6px alt-font">analysis.</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
