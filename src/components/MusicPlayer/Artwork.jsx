54bd843bd3aa6bd91866773706d6a252d32e0d79
jackvdp <jack@pumpymusic.co.uk>
2024-10-13 22:48:46 +0100

    Make image not draggable

--- src/components/MusicPlayer/Artwork.jsx
+++ src/components/MusicPlayer/Artwork.jsx
@@ -7,11 +7,13 @@ export default function Artwork({currentTrack, isPlaying, handleArtworkClick}) {
                 src={currentTrack.picture}
                 alt={`${currentTrack.title} Artwork`}
                 className={styles.artwork}
+                draggable="false"
             />
             <img
                 src="/imagesNew/vinyl.png"
                 alt="Vinyl"
                 className={`${styles.vinyl} ${styles.spin} ${isPlaying && styles.expose}`}
+                draggable="false"
             />
         </div>
     )
