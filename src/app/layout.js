a6ccd705ee946cab1e474e1c1f4ab806126f70bc
jackvdp <jack@pumpymusic.co.uk>
2024-10-13 12:53:35 +0100

    Use inter font across site

--- src/app/layout.js
+++ src/app/layout.js
@@ -24,6 +24,7 @@ export default function RootLayout({ children }) {
         <link rel="icon" type="image/png" sizes="16x16" href="favicon/favicon-16x16.png" />
         <link rel="preconnect" href="https://fonts.googleapis.com" crossOrigin="true" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Inter:wght@200;300;400;500;600;700;800;900&display=swap" />
       </head>
       <body>
         <MusicPlayerProvider>
