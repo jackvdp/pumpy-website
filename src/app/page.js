186a632a2f31eab3adfb8c113f2bb4c518af4de7
jackvdp <jack@pumpymusic.co.uk>
2024-10-21 14:46:49 +0100

    Use next js script tags

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,6 @@
 "use client"
 
 import Navbar from "@/components/Navbar";
-import { useEffect, useRef } from "react";
-import loadScript from "@/utils/loadScript";
 import Hero from "@/components/Hero";
 import Logos from "@/components/Logos";
 import About from "@/components/About";
@@ -14,37 +12,6 @@ import Marquee from "@/components/Marquee";
 import Footer from "@/components/Footer";
 
 export default function Home() {
-  const scriptsLoadedRef = useRef(false);
-
-  useEffect(() => {
-    const loadScripts = async () => {
-      try {
-        await loadScript('/js/jquery.js');
-        await loadScript('/js/vendors.js');
-        await loadScript('/js/main.js');
-      } catch (error) {
-        console.error('Error loading scripts:', error);
-      }
-    };
-
-    if (!scriptsLoadedRef.current) {
-      scriptsLoadedRef.current = true;
-      loadScripts();
-    }
-
-    // Cleanup function
-    return () => {
-      if (typeof window !== 'undefined') {
-        ['jquery.js', 'vendors.min.js', 'main.js'].forEach(src => {
-          const script = document.querySelector(`script[src="/js/${src}"]`);
-          if (script) {
-            document.body.removeChild(script);
-          }
-        });
-        scriptsLoadedRef.current = false;
-      }
-    };
-  }, []);
 
   return (
     <>
