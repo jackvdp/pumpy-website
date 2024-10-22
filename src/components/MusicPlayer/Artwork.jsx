3bbe3e6c6adff83ba4c27ad2556edf3a667ecafb
jackvdp <jack@pumpymusic.co.uk>
2024-10-22 21:37:44 +0100

    Hide image before image is loading

--- src/components/MusicPlayer/Artwork.jsx
+++ src/components/MusicPlayer/Artwork.jsx
@@ -1,16 +1,24 @@
+import React from 'react';
 import styles from './Artwork.module.css';
 import Image from 'next/image';
 
 export default function Artwork({ currentTrack, isPlaying, showControls, handleArtworkClick }) {
+    const [isLoaded, setIsLoaded] = React.useState(false);
+
+    const handleImageLoad = () => {
+        setIsLoaded(true);
+    };
+
     return (
-        <div className={styles.artworkContainer} onClick={handleArtworkClick}>
+        <div className={(isLoaded ? ` ${styles.artworkContainer}` : ` ${styles.hidden}`)} onClick={handleArtworkClick}>
             <Image
                 src={currentTrack.picture}
                 alt={`${currentTrack.title} Artwork`}
                 width={100}
                 height={100}
-                className={styles.artwork}
+                className={styles.artwork + (isLoaded ? `` : ` ${styles.hidden}`)}
                 draggable="false"
+                onLoadingComplete={handleImageLoad}
             />
             <span className={`${styles.icon} ${isPlaying && !showControls ? styles.hidden : ''}`}>
                 <i className={"fa-solid " + (isPlaying ? "fa-pause" : "fa-play")}></i>
