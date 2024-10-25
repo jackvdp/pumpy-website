5cae100066dc8d5d32c3e8a59b8766496f091ac1
jackvdp <jack@pumpymusic.co.uk>
2024-10-25 16:09:26 +0100

    Correctly route favicons

--- src/app/layout.js
+++ src/app/layout.js
@@ -13,23 +13,38 @@ import Head from "next/head"
 export const metadata = {
   title: "Vanderpump Tech | Web & App Development",
   description: "Bespoke Web & App Development Solutions",
+  icons: {
+    icon: '/favicon/favicon-32x32.png',
+    shortcut: '/favicon/favicon.ico',
+    apple: '/favicon/apple-touch-icon.png',
+    other: [
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '16x16',
+        url: '/favicon/favicon-16x16.png',
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '32x32',
+        url: '/favicon/favicon-32x32.png',
+      },
+    ],
+  },
 };
 
 export default function RootLayout({ children }) {
   return (
     <html className="no-js" lang="en">
-      <Head>
+      <head>
         <meta charSet="utf-8" />
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width,initial-scale=1.0" />
-        <link rel="shortcut icon" href="favicon/favicon.ico" />
-        <link rel="apple-touch-icon" sizes="180x180" href="favicon/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="favicon/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="favicon/favicon-16x16.png" />
         <link rel="preconnect" href="https://fonts.googleapis.com" crossOrigin="true" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
         <link rel="preload" href="https://fonts.googleapis.com/css2?family=Inter:wght@200;300;400;500;600;700;800;900&display=swap" as="style" />
-      </Head>
+      </head>
       <body data-mobile-nav-style="full-screen-menu" data-mobile-nav-bg-color="#2d2c2b" className="custom-cursor">
         <MusicPlayerProvider>
           <CustomCursor />
