eafc3ebf40c67f6704a00065fb27246fe13600c7
jackvdp <jack@pumpymusic.co.uk>
2024-10-25 17:00:39 +0100

    Extract component to another file

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,5 @@
 import Image from "next/image"
+import ImagesSlider from "./ImageSliders"
 
 export default function Hero() {
     return (
@@ -56,65 +57,4 @@ export default function Hero() {
             </div>
         </section >
     )
-}
-
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { Navigation, EffectFade, Autoplay } from 'swiper/modules';
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/effect-fade';
-
-function ImagesSlider() {
-    return (
-        <Swiper
-            modules={[Navigation, EffectFade, Autoplay]}
-            className="h-100 banner-slider sm-h-450px"
-            slidesPerView={1}
-            effect="fade"
-            loop={true}
-            speed={1000}
-            navigation={{
-                nextEl: '.slider-one-slide-next-1',
-                prevEl: '.slider-one-slide-prev-1',
-            }}
-            autoplay={{
-                delay: 4000,
-                disableOnInteraction: false,
-                pauseOnMouseEnter: true
-            }}
-        >
-            <SwiperSlide>
-                <Image
-                    priority
-                    fill
-                    className="position-absolute left-0px top-0px w-100 h-100 object-cover"
-                    src='/imagesNew/hero/hero1.webp'
-                    alt="code"
-                />
-            </SwiperSlide>
-            <SwiperSlide>
-                <Image
-                    fill
-                    className="position-absolute left-0px top-0px w-100 h-100 object-cover"
-                    src='/imagesNew/hero/hero2.webp'
-                    alt="code"
-                />
-            </SwiperSlide>
-            <SwiperSlide>
-                <Image
-                    fill
-                    className="position-absolute left-0px top-0px w-100 h-100 object-cover"
-                    src='/imagesNew/hero/hero3.webp'
-                    alt="code"
-                />
-            </SwiperSlide>
-
-            <div className="slider-one-slide-prev-1 icon-very-small text-white swiper-button-prev slider-navigation-style-06 d-none d-sm-inline-block">
-                <i className="line-icon-Arrow-OutLeft icon-extra-large"></i>
-            </div>
-            <div className="slider-one-slide-next-1 icon-very-small text-white swiper-button-next slider-navigation-style-06 d-none d-sm-inline-block">
-                <i className="line-icon-Arrow-OutRight icon-extra-large"></i>
-            </div>
-        </Swiper>
-    );
 }
