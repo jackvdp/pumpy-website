32409115f01cbe82f8600e99fb48d2365abf7487
jackvdp <jack@pumpymusic.co.uk>
2025-04-07 09:40:06 +0100

    Fix contact form

--- src/components/Contact/ContactForm.jsx
+++ src/components/Contact/ContactForm.jsx
@@ -1,34 +1,66 @@
+"use client";
+
+import React from 'react';
+import {useForm, ValidationError} from '@formspree/react';
+
 export default function ContactForm() {
-    return <form action="https://formspree.io/f/mzzbzqjz" method="post">
-        <div className="position-relative form-group mb-20px">
-            <input
-                className="ps-0 border-radius-0px medium-gray bg-transparent border-color-light-red form-control required"
-                type="text"
-                name="name"
-                placeholder="Enter your name*"
-            />
-        </div>
-        <div className="position-relative form-group mb-20px">
-            <input
-                className="ps-0 border-radius-0px medium-gray bg-transparent border-color-light-red form-control required"
-                type="email"
-                name="email"
-                placeholder="Enter your email*"
-            />
-        </div>
-        <div className="position-relative z-index-1 form-group form-textarea mt-15px mb-0">
-            <textarea
-                className="ps-0 border-radius-0px medium-gray bg-transparent border-color-light-red form-control"
-                name="comment"
-                placeholder="Enter your message"
-                rows="3"
-            />
-            <input type="hidden" name="redirect" value=""/>
-            <button
-                className="btn btn-medium btn-dark-gray btn-round-edge btn-box-shadow mb-20px mt-25px submit w-100"
-                type="submit">Send message<i className="fa-regular fa-envelope"></i>
-            </button>
-            <div className="form-results mt-20px d-none"></div>
-        </div>
-    </form>;
+    const [state, handleSubmit] = useForm("mzzbzqjz");
+
+    if (state.succeeded) {
+        return <p>Thanks for your message! We&#39;ll be in touch shortly</p>;
+    }
+
+    console.log("*****", state);
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <div className="position-relative form-group mb-20px">
+                <input
+                    className="ps-0 border-radius-0px medium-gray bg-transparent border-color-light-red form-control required"
+                    type="text"
+                    name="name"
+                    placeholder="Enter your name*"
+                />
+                <ValidationError
+                    prefix="Name"
+                    field="name"
+                    errors={state.errors}
+                />
+            </div>
+            <div className="position-relative form-group mb-20px">
+                <input
+                    className="ps-0 border-radius-0px medium-gray bg-transparent border-color-light-red form-control required"
+                    type="email"
+                    name="email"
+                    placeholder="Enter your email*"
+                />
+                <ValidationError
+                    prefix="Email"
+                    field="email"
+                    errors={state.errors}
+                />
+            </div>
+            <div className="position-relative z-index-1 form-group form-textarea mt-15px mb-0">
+        <textarea
+            className="ps-0 border-radius-0px medium-gray bg-transparent border-color-light-red form-control"
+            name="comment"
+            placeholder="Enter your message"
+            rows="3"
+        />
+                <ValidationError
+                    prefix="Comment"
+                    field="comment"
+                    errors={state.errors}
+                />
+                <button
+                    className="btn btn-medium btn-dark-gray btn-round-edge btn-box-shadow mb-20px mt-25px w-100"
+                    type="submit"
+                    disabled={state.submitting}
+                >
+                    Send message<i className="fa-regular fa-envelope"></i>
+                </button>
+                <div className="form-results mt-20px d-none"></div>
+            </div>
+        </form>
+    );
 }
